753a9b9aaa90ffd3b420ac71263ca88e7d50cac8
Cristina Zhang <rami0141@umn.edu>
2018-06-06 16:33:17 -0500

    added components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   // this is the info angular needsto be able to replace the <app-root> with the template of this component inside app.component.html
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // can add multiple stylesheets here
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // each component needs to have it's own template
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
