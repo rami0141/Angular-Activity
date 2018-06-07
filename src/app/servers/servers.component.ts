4b965584b9854d83cd7a5294b1ece10d2b975f66
Cristina Zhang <rami0141@umn.edu>
2018-06-07 14:49:57 -0500

    events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  // don't want to allow the user to create a new server
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!"
+  serverName = '';
 
-  constructor() { }
+  constructor() {
+    // after 2000 milliseconds = 2 seconds
+    setTimeout(() => {
+    this.allowNewServer = true;
+  }, 2000)
+  }
 
   ngOnInit() {
   }
-
+  onCreateServer() {
+    this.serverCreationStatus = ' Server was created!'
+  }
+  onUpdateServerName(event: Event) {
+    // Output what the user typed
+    // will be an HTML input element
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
